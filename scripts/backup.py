8d58fd237959d81490e1f0893635591a3064b41a
rachel3834 <rstreet@lcogt.net>
2017-06-23 17:34:30 -0700

    Added backup script

---
+++ scripts/backup.py
@@ -0,0 +1,52 @@
+from os import path, makedirs, chdir, getcwd
+from sys import exit, stdout
+import logging
+import subprocess
+from datetime import datetime
+import log_utilities
+import config_parser
+import socket
+
+def backup_database():
+    """Function to automatically back-up the TOM database"""
+    
+    config = read_config()
+    
+    log = log_utilities.start_day_log( config, config['log_root_name'] )
+
+    log.info('Executing rsync command: '+config['rsync_command'])
+    child = subprocess.Popen(config['rsync_command'].split(' '),
+                                  shell=False, stderr=subprocess.PIPE)
+    while True:
+        err = child.stderr.readlines()
+        for e in err:
+            log.info(e)
+        if child.poll() != None:
+            break
+    
+    log.info('Completed DB back-up')
+    log_utilities.end_day_log(log)
+
+def read_config():
+    """Function to read the back-up script configuration file.
+    Returns a dictionary of the config parameters from the XML file. 
+    """
+
+    config = config_parser.read_config_for_code('backup')
+
+    ts = datetime.utcnow()
+    ts = ts.strftime("%Y-%m-%dT%H:%M:%S")
+    db_path = config['db_location']
+    db_file = path.basename(db_path)+'_'+ts
+    bkup_path = path.join(config['backup_dir'], db_file)
+    config['rsync_command'] = 'rsync -a ' + db_path + ' ' + bkup_path
+
+    if path.isdir(config['backup_dir']) == False:
+        makedirs(config['backup_dir'])
+    if path.isdir(config['log_dir']) == False:
+        makedirs(config['log_dir'])
+
+    return config
+
+if __name__ == '__main__':
+    backup_database()
---
+++ scripts/config_parser.py
@@ -0,0 +1,76 @@
+import xml.sax.handler
+from os import path
+from sys import argv
+import socket
+
+class ConfigHandler(xml.sax.handler.ContentHandler):
+    """Configuation Handler class for generic XML files in 
+    parameter:value pair format."""
+
+    def __init__(self):
+        self.ivalue = False
+        self.par_name = None
+        self.par_value = None
+        self.mapping = {}
+
+    def startElement(self,name,attributes):
+        if name == 'parameter':
+            self.par_name = attributes['name']
+        elif name == 'value':
+            self.ivalue = True
+
+    def characters(self,data):
+        if self.ivalue == True: self.par_value = data
+
+    def endElement(self,name):
+        if name == 'value':
+            self.ivalue = False
+            self.mapping[self.par_name] = self.par_value
+
+def read_config(config_file_path):
+
+    config = {}
+    if path.isfile(config_file_path) == False: return config
+
+    parser = xml.sax.make_parser( )
+    config_handler = ConfigHandler( )
+
+    parser.setContentHandler(config_handler)
+    parser.parse(config_file_path)
+    config = config_handler.mapping
+
+    return config
+
+def read_config_for_code(code_name):
+    """Function to read XML configuration files
+    code_name = { 'backup' }    
+    """
+    
+    configs = { 'backup': 'backup_config.xml' }
+    if code_name in configs.keys():
+        config_file = configs[code_name]
+    
+    host_name = socket.gethostname()
+    if 'rachel' in str(host_name).lower():
+        config_file_path = path.join('/Users/rstreet/software/mulens_tom/configs/',\
+                                         configs[code_name])
+    else:
+        config_file_path = path.join('/var/www/spitzermicrolensing/configs/',configs[code_name])
+    
+    if path.isfile(config_file_path) == False:
+        raise IOError('Cannot find configuration file, looking for:'+config_file_path)
+    script_config = read_config(config_file_path)
+    
+    return script_config
+
+if __name__ == '__main__':
+
+    if len(argv) > 1: config_file_path = argv[1]
+    else: config_file_path = raw_input('Please input path to an XML config file: ')
+
+    config = read_config(config_file_path)
+
+    if config == None: print 'ERROR: Cannot read file '+config_file_path
+    else:
+        print 'Input configuration:'
+        for par,par_value in config.items(): print '    ',par, ':', par_value
